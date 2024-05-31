b544d758614771e9b7ed85b9f70174f554768087
Timo Cuijpers <timo@uit.best>
2024-05-31 13:31:40 +0200

    Update packages

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,14 +4,15 @@ import _ from 'lodash';
 import vuetify from '@/js/plugins/vuetify';
 import router from '@/js/router';
 import pinia from '@/js/plugins/pinia';
-
-// Vuetify
 import 'vuetify/styles';
+// import api from '@weapnl/js-junction';
 
 const app = createApp(App);
 
 app.config.globalProperties.$_ = _;
 window._ = _;
+
+// api.host(import.meta.env.VITE_API_URL);
 
 app.use(router);
 app.use(vuetify);
